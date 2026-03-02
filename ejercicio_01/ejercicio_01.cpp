ee48ae5dfaf62bc7f6bc5e19252c3c9b07e032ff
juanbetancourt01 <camilin1869@gmail.com>
2026-03-02 08:02:18 -0500

    feat: resuelto ejercicio 01 - conversor de temperatura

---
+++ ejercicio_01/ejercicio_01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    double celsius;
+
+    std::cout << "Ingrese temperatura en Celsius: ";
+    std::cin >> celsius;
+
+    double fahrenheit = (celsius * 9/5) + 32;
+    double kelvin = celsius + 273.15;
+    double rankine = (celsius + 273.15) * 9/5;
+
+    std::cout << "Fahrenheit: " << fahrenheit << std::endl;
+    std::cout << "Kelvin: " << kelvin << std::endl;
+    std::cout << "Rankine: " << rankine << std::endl;
+
+    return 0;
+}
