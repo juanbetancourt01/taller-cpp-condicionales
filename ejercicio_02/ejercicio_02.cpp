55da4c4301c691e72c2ba10d277e126969faf52b
juanbetancourt01 <camilin1869@gmail.com>
2026-03-02 08:07:41 -0500

    feat: resuelto ejercicio 02 - clasificador de triangulos

---
+++ ejercicio_02/ejercicio_02.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+int main() {
+    double a, b, c;
+
+    std::cout << "Ingrese lado 1: ";
+    std::cin >> a;
+    std::cout << "Ingrese lado 2: ";
+    std::cin >> b;
+    std::cout << "Ingrese lado 3: ";
+    std::cin >> c;
+
+    // Verificar si es triangulo valido
+    if (a + b > c && a + c > b && b + c > a) {
+
+        std::cout << "Es un triangulo valido\n";
+
+        // Clasificacion por lados
+        if (a == b && b == c) {
+            std::cout << "Tipo: Equilatero\n";
+        } 
+        else if (a == b  a == c 
+ b == c) {
+            std::cout << "Tipo: Isosceles\n";
+        } 
+        else {
+            std::cout << "Tipo: Escaleno\n";
+        }
+
+        // Clasificacion por angulos
+        double a2 = a * a;
+        double b2 = b * b;
+        double c2 = c * c;
+
+        if (a2 + b2 == c2  a2 + c2 == b2 
+ b2 + c2 == a2) {
+            std::cout << "Es Rectangulo\n";
+        } 
+        else if (a2 + b2 > c2 && a2 + c2 > b2 && b2 + c2 > a2) {
+            std::cout << "Es Acutangulo\n";
+        } 
+        else {
+            std::cout << "Es Obtusangulo\n";
+        }
+
+    } 
+    else {
+        std::cout << "No es un triangulo valido\n";
+    }
+
+    return 0;
+}
